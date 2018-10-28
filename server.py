1a2dfe4fe65c4b384b7503fc3754ed08effb3b2b
Raquel Chozas <r.chozas@alumnos.urjc.es>
2018-10-28 13:54:32 +0100

    6 server

--- server.py
+++ server.py
@@ -11,20 +11,28 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    dicc_usuario = {}
+    
     def handle(self):
         IP = self.client_address[0]
         print("IP cliente: ", IP)
         PORT = self.client_address[1]
         print("Puerto: ", str(PORT))
         
-        """
-        handle method of the server class
-        (all requests will be handled by this method)
-        """
-        self.wfile.write(b"Hemos recibido tu peticion")
-        for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+        line = self.rfile.read()
+
+        info = line.decode('utf-8')
+        
+        if (len(info) >= 2):
+            if (info.split()[0].upper() == "REGISTER"):
+                self.dicc_usuario[info.split()[1]] = self.client_address[0]
+                self.wfile.write(b"Hemos recibido tu peticion\r\n\r\n")
+            
+            print(self.dicc_usuario)
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+  
+                  
+        
 
 if __name__ == "__main__":
 
